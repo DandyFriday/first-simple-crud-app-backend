f09c5ec0ac42fc0924559edf8cbb496019812d1b
DandyGeorge <dandyfriday31@gmail.com>
2025-08-16 03:15:26 +0100

    Health check added

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('./models/ProductModel.js');
 const dotenv = require('dotenv');
 const ProductRoute = require('./routes/ProductRoute.js');
-
-
 
 dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
-//Middleware
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-//Routes
-app.use('/api/products', ProductRoute)
+// Routes
+app.use('/api/products', ProductRoute);
 
-
+// Health check route
 app.get('/', (req, res) => {
-    res.send('Hello from Node API practice Server');
+    res.json({
+        status: true,
+        message: 'Health check pass'
+    });
 });
 
-//connection to mongodb    9JuF5CtSGKqefhYOe  dandyfriday31
+// Connection to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to database!');
@@ -34,3 +35,4 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch((err) => {
     console.log('Connection failed:', err);
   });
+
